18066f9b54e54de27a07b139d8142cddf50782bf
JamesByron <jamesbyron84@gmail.com>
2018-03-11 12:31:09 -0700

    Changed files

--- AVX_examples/hello_avx.c
+++ AVX_examples/hello_avx.c
@@ -1,8 +1,17 @@
 #include <immintrin.h>
 #include <stdio.h>
 
-int main() {
+struct AVXVec {
+  struct AVXVec *left;
+  struct AVXVec *right;
+  __m256 data;
+  __m256 valid; 
+  int pin;
+  };
 
+int main() {
+  struct AVXVec avx;
+  avx.pin = 0;
   /* Initialize the two argument vectors */
   __m256 evens = _mm256_set_ps(2.0, 4.0, 6.0, 8.0, 10.0, 12.0, 14.0, 16.0);
   __m256 odds = _mm256_set_ps(1.0, 3.0, 5.0, 7.0, 9.0, 11.0, 13.0, 15.0);
@@ -17,3 +26,8 @@ int main() {
 
   return 0;
 }
+//_mm256_max_epi16
+//_mm256_movemask_epi8 : get an int with bits set by the first of each 32 bit value
+//_mm256_cmpgt_epi32 : compare values, setting result to 1 if true 
+//_mm256_blendv_epi8
+//https://software.intel.com/sites/landingpage/IntrinsicsGuide/#techs=AVX2&expand=408,742,750,3316,3610,5113,5154
