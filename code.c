8c4b3d1cfe2524574df8ae4cdd34a36335a9e027
James Byron <jbyron@ucsc.edu>
2018-03-13 03:10:43 -0700

    Implemented sorting

--- code.c
+++ code.c
@@ -6,3 +6,10 @@ struct AVXVec {
 	_m256epsi valid; 
 	int pin;
 	};
+
+struct AVXVec {
+  struct AVXVec *child;
+  __m256i *data;
+  int dataLength; // The number of vectors in each row of data[]
+  int currentDataIndex; // the current index in data[] that we can fill, always < 10
+  };
